96c3cb7dd7f551be68e7e99455fafc2eb889fecf
Łukasz Modzelewski <lukasz.modzelewski1@gmail.com>
2020-07-25 02:07:23 +0200

    Animated GIF saving.

--- 3DRenderer.cpp
+++ 3DRenderer.cpp
@@ -3,7 +3,7 @@
 #include <cmath>
 #include <iostream>
 #include <limits>
-#include <vector>
+#include "gif.h"
 
 struct Circle
 {
@@ -27,68 +27,87 @@ int main()
 {
     const int width = 800, height = 600;
     const float fov = M_PI / 3.f;
-    Vec3f *pixels = new Vec3f[width * height], *p = pixels;
+    uint8_t *gifBuffer = new uint8_t[width * height * 4], *p;
+    GifWriter g;
+    const int delay = 3;
+	GifBegin(&g, "output.gif", width, height, delay);
 
     const int noOfCircles = 3;
     Circle *circles[3];
 
-    circles[0] = new Circle(Vec3f(-1.0, -1.5, -12), 0.25, Vec3f(2, 3, 1).normalize(), Vec3f(0.3, 0.1, 0.1));
+    circles[0] = new Circle(Vec3f(-5.0, 5, -12), 2, Vec3f(0, 1, 0).normalize(), Vec3f(0.3, 0.1, 0.1));
     circles[1] = new Circle(Vec3f(7, 5, -18), 0.5, Vec3f(1, 2, 0).normalize(), Vec3f(1.0, 1.0, 1.0));
     circles[2] = new Circle(Vec3f(-2, -3, -15), 1, Vec3f(2, 1, 0).normalize(), Vec3f(0.4, 0.4, 0.3));
 
     Vec3f hit, N, color, orig(0, 0, 0), dir(0, 0, -height / (2.f * tan(fov / 2.f)));
-    int y, x, i;	
-    for(y = 0; y < height; ++y)
+    int y, x, i;
+
+    const float velocity = 0.1f;
+    while(circles[0]->Center.x < 5.f)
     {
-        for(x = 0; x < width; ++x)
+        p = gifBuffer;
+        for(y = 0; y < height; ++y)
         {
-            dir.x =  (x + 0.5) -  width / 2.f;
-            dir.y = -(y + 0.5) + height / 2.f;
-            
-            float shapes_dist = std::numeric_limits<float>::max();
-            for (i = 0; i < noOfCircles; ++i)
+            for(x = 0; x < width; ++x)
             {
-                float d;
-                if (circles[i]->ray_intersect(orig, dir, d) && d < shapes_dist)
+                dir.x =  (x + 0.5) -  width / 2.f;
+                dir.y = -(y + 0.5) + height / 2.f;
+                
+                float shapes_dist = std::numeric_limits<float>::max();
+                for (i = 0; i < noOfCircles; ++i)
                 {
-                    shapes_dist = d;
-                    hit = orig + dir * d;
-                    if(hit * circles[i]->Normal > 0)
-                        N = -circles[i]->Normal;
-                    else
-                        N = circles[i]->Normal;
-                    color = circles[i]->Color;
+                    float d;
+                    if (circles[i]->ray_intersect(orig, dir, d) && d < shapes_dist)
+                    {
+                        shapes_dist = d;
+                        hit = orig + dir * d;
+                        if(hit * circles[i]->Normal > 0)
+                            N = -circles[i]->Normal;
+                        else
+                            N = circles[i]->Normal;
+                        color = circles[i]->Color;
+                    }
                 }
-            }
 
-            float checkerboard_dist = std::numeric_limits<float>::max();
-            if (fabs(dir.y) > 1e-3)
-            {
-                float d = -(orig.y + 4)/dir.y; // the checkerboard plane has equation y = -4
-                Vec3f pt = orig + dir * d;
-                if (d > 0 && fabs(pt.x) < 10 && pt.z < -10 && pt.z > -30 && d < shapes_dist)
+                float checkerboard_dist = std::numeric_limits<float>::max();
+                if (fabs(dir.y) > 1e-3)
                 {
-                    checkerboard_dist = d;
-                    hit = pt;
-                    N = Vec3f(0, 1, 0);
-                    color = (int(.5 * hit.x + 1000) + int(.5 * hit.z)) & 1 ? Vec3f(.3, .3, .3) : Vec3f(.3, .2, .1);
+                    float d = -(orig.y + 4)/dir.y; // the checkerboard plane has equation y = -4
+                    Vec3f pt = orig + dir * d;
+                    if (d > 0 && fabs(pt.x) < 10 && pt.z < -10 && pt.z > -30 && d < shapes_dist)
+                    {
+                        checkerboard_dist = d;
+                        hit = pt;
+                        N = Vec3f(0, 1, 0);
+                        color = (int(.5 * hit.x + 1000) + int(.5 * hit.z)) & 1 ? Vec3f(.3, .3, .3) : Vec3f(.3, .2, .1);
+                    }
                 }
+                if(std::min(shapes_dist, checkerboard_dist) < 1000)
+                {
+                    *(p++) = 256 * color.x;
+                    *(p++) = 256 * color.y;
+                    *(p++) = 256 * color.z;
+                }
+                else
+                {
+                    //background color
+                    *(p++) = 256 * 0.f;//r
+                    *(p++) = 256 * 0.3f;//g
+                    *(p++) = 256 * 0.2f;//b
+                }
+                ++p;//alpha is ignored
             }
-            if(std::min(shapes_dist, checkerboard_dist) < 1000)//return true
-                *p = color;//operator=
-            else//return false
-            {
-                p->x = 0.f; p->y = 0.3f; p->z = 0.2f;//background color
-            }
-            ++p;
         }
+        GifWriteFrame(&g, gifBuffer, width, height, delay);
+        circles[0]->Center.x += velocity;
+        //orig.y += velocity;
+        orig.z -= velocity*0.5f;
     }
-
     for(i = 0; i < noOfCircles; ++i)
         delete circles[i];
 
-    BMPSaver("output", width, height, pixels).Save();
+    GifEnd(&g);
+    delete[] gifBuffer;
 
-    delete[] pixels;
     return 0;
 }
