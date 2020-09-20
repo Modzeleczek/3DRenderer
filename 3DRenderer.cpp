8fbc0c54e57aa527fc3458729045936b98460a8d
Łukasz Modzelewski <lukasz.modzelewski1@gmail.com>
2020-09-20 03:12:22 +0200

    Replace getters with const variables and use initializer list in Renderer. Save constant frame height in Camera.

--- 3DRenderer.cpp
+++ 3DRenderer.cpp
@@ -143,19 +143,21 @@ private:
         // rotated coordinate system.
         // distance between camera and screen depending on field of view
         float ScreenDistance;
+        const int FrameHeight;
 
     public:
         Camera(uint32_t frameHeight = 512, float fieldOfView = M_PI / 3.f, const Vec3f &position = Vec3f(0,0,0))
+            : FrameHeight(frameHeight)
         {
             Position = position;
             Direction = Vec3f(0,0,-1);
             HorizontalAxis = Vec3f(1,0,0);
             VerticalAxis = Vec3f(0,1,0);
-            SetFieldOfView(frameHeight, fieldOfView);
+            SetFieldOfView(fieldOfView);
         }
-        void SetFieldOfView(int frameHeight, float fieldOfView)
+        void SetFieldOfView(float fieldOfView)
         {
-            ScreenDistance = frameHeight / (2.f * tan(fieldOfView / 2.f));
+            ScreenDistance = FrameHeight / (2.f * tan(fieldOfView / 2.f));
         }
         void RotateX(float angle)
         {
@@ -187,21 +189,16 @@ private:
         }
     };
 
-    int Width, Height;
-    byte *FrameBuffer;
-
 public:
+    const int Width, Height;
+    byte *const FrameBuffer;
     std::vector<Shape*> Shapes;
-
     Camera Eye;
-    Renderer(uint32_t frameWidth = 512, uint32_t frameHeight = 512)
-    {
-        Width = frameWidth;
-        Height = frameHeight;
-        FrameBuffer = new byte[Width * Height * 4]; // 4 bytes per pixel (RGBA)
 
-        Eye = Camera(frameHeight);
-    }
+    Renderer(uint32_t frameWidth = 512, uint32_t frameHeight = 512)
+        : Width(frameWidth), Height(frameHeight), 
+          FrameBuffer(new byte[Width * Height * 4]), // 4 bytes per pixel (RGBA)
+          Eye(frameHeight) {}
     ~Renderer()
     {
         if(FrameBuffer)
@@ -210,11 +207,6 @@ public:
             if(Shapes[i])
                 delete Shapes[i];
     }
-
-    //    int & Width()       { return Width_; } // get, set
-    const int & GetWidth() const { return Width; } // get only
-    const int & GetHeight() const { return Height; }
-    const byte* GetFrameBuffer() const { return FrameBuffer; }
 
     void RenderFrame()
     {
@@ -272,7 +264,7 @@ int main()
     Renderer renderer;
     GifWriter writer;
     const uint32_t delay = 5;
-	GifBegin(&writer, "output.gif", renderer.GetWidth(), renderer.GetHeight(), delay);
+	GifBegin(&writer, "output.gif", renderer.Width, renderer.Height, delay);
 
     srand(time(0));
     renderer.Shapes.push_back(new Circle(Vec3f(6,3,-12), 3, Vec3f(1,1,0).normalize(), randomColor()));
@@ -287,16 +279,14 @@ int main()
     // Negative angle rotates clockwise.
     renderer.Eye.RotateY(-M_PI / 2);
 
-    const uint32_t totalFrames = 256;
+    const uint32_t totalFrames = 128;
     const float rotationVelocity = M_PI * 1.f / (float) totalFrames;
-    uint32_t frameCounter = 0;
-    while(frameCounter < totalFrames)
+    for(uint32_t frameCounter = 0; frameCounter < totalFrames; ++frameCounter)
     {
         renderer.RenderFrame();
-        GifWriteFrame(&writer, renderer.GetFrameBuffer(), 
-            renderer.GetWidth(), renderer.GetHeight(), delay);
+        GifWriteFrame(&writer, renderer.FrameBuffer, 
+            renderer.Width, renderer.Height, delay);
         renderer.Eye.RotateY(rotationVelocity);
-        ++frameCounter;
     }
     GifEnd(&writer);
 
