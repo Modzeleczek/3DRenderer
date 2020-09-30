76ea7f8383cee4eb2ce52ca2519a203aa785e151
Łukasz Modzelewski <lukasz.modzelewski1@gmail.com>
2020-09-30 05:10:31 +0200

    Prepare an animation demo in Main.cpp.

--- Main.cpp
+++ Main.cpp
@@ -11,7 +11,7 @@ inline Vec3b randomColor()
 
 int main()
 {
-    Renderer renderer;
+    Renderer renderer(256, 256);
     GifWriter writer;
     const uint32_t delay = 5;
 	GifBegin(&writer, "output.gif", renderer.Width, renderer.Height, delay);
@@ -23,36 +23,45 @@ int main()
     Material     mirror(1.0, Vec4f(0.0, 10.0, 0.8, 0.0), Vec3f(1.0, 1.0, 1.0), 1425.);
 
     srand(time(0));
-    renderer.Shapes.push_back(new Circle(Vec3f(-2,0,-10), 0.5, Vec3f(0,1,1).Normalize(), 
-        ivory));
+    // walls
     renderer.Shapes.push_back(new Plane(Vec3f(-6,0,-20), Vec3f(1,0,0).Normalize(), 
-        glass));
+        red_rubber));
     renderer.Shapes.push_back(new Plane(Vec3f(5,0,-15), Vec3f(0,0,1).Normalize(), 
         red_rubber));
     renderer.Shapes.push_back(new Plane(Vec3f(0,-4,0), Vec3f(0,1,0).Normalize(), 
-        mirror));
-    renderer.Shapes.push_back(new Rectangle(Vec3f(0,0,-10), 0.5, 0.5, Vec3f(1,1,1).Normalize(), 
-        ivory));
-    renderer.Shapes.push_back(new Sphere(Vec3f(2,0,-10), 0.5, 
-        glass));
-    renderer.Shapes.push_back(new Ellipse(Vec3f(4,0,-10), Vec3f(6,0,-10), 1, Vec3f(0,0,1).Normalize(), 
         red_rubber));
 
-    // Negative angle rotates clockwise.
-    //renderer.Eye.RotateY(-M_PI / 2);
+    // shapes
+    renderer.Shapes.push_back(new Circle(Vec3f(-3,0,-10), 2, Vec3f(0,1,1).Normalize(), 
+        ivory));
+    renderer.Shapes.push_back(new Rectangle(Vec3f(3,0,-5), 4, 4, Vec3f(0,0,1).Normalize(), 
+        glass));
+    renderer.Shapes.push_back(new Sphere(Vec3f(3,0,-10), 2, 
+        mirror));
+    // renderer.Shapes.push_back(new Ellipse(Vec3f(6,0,-10), Vec3f(6,0,-10), 1, Vec3f(0,0,1).Normalize(), 
+    //    ivory));
 
-    const uint32_t totalFrames = 1;
-    /*const float rotationVelocity = M_PI * 1.f / (float) totalFrames;
-    float targetX = -5.f / 2.f;
-    const float velocity = 5.f / totalFrames;*/
+    renderer.Lights.push_back(new Light(Vec3f(-5, 10,  -1), 1.5));
+    renderer.Lights.push_back(new Light(Vec3f( 5, 10, -1), 1.8));
+    // renderer.Lights.push_back(new Light(Vec3f( 30, 20,  -1), 1.7));
+
+    // Negative angle rotates clockwise.
+    // renderer.Eye.RotateY(-M_PI / 2);
+
+    const uint32_t totalFrames = 32;
+    // const float rotationVelocity = M_PI * 1.f / (float) totalFrames;
+    // float targetX = -5.f / 2.f;
+    const float velocity = 5.f / totalFrames;
     for(uint32_t frameCounter = 0; frameCounter < totalFrames; ++frameCounter)
     {
-        //renderer.Eye.SetDirection(Vec3f(targetX,0,-10).Normalize());
+        // renderer.Eye.SetDirection(Vec3f(targetX,0,-10).Normalize());
         renderer.RenderFrame();
         GifWriteFrame(&writer, renderer.FrameBuffer, 
             renderer.Width, renderer.Height, delay);
-        //renderer.Eye.RotateY(rotationVelocity);
-        //targetX += velocity;
+        // renderer.Eye.RotateY(rotationVelocity);
+        // targetX += velocity;
+        // dynamic_cast<Circle*>(renderer.Shapes[3])->SetDirection
+        renderer.Shapes[3]->Center.X += velocity;
     }
     GifEnd(&writer);
 
