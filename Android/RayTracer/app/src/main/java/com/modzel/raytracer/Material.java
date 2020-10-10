64bc5e90af805ee66952a27f2f479f8ab6fa8f33
Łukasz Modzelewski <lukasz.modzelewski1@gmail.com>
2020-10-10 04:10:30 +0200

    Add Material copying constructor and missing copying constructors' calls in the standard constructor.

--- Android/RayTracer/app/src/main/java/com/modzel/raytracer/Material.java
+++ Android/RayTracer/app/src/main/java/com/modzel/raytracer/Material.java
@@ -9,8 +9,8 @@ public class Material {
 
     public Material(float refractiveIndex, Vec4f albedo, Vec3f color, float specularExponent) {
         RefractiveIndex = refractiveIndex;
-        Albedo = albedo;
-        DiffuseColor = color;
+        Albedo = new Vec4f(albedo);
+        DiffuseColor = new Vec3f(color);
         SpecularExponent = specularExponent;
     }
     public Material() {
@@ -18,5 +18,11 @@ public class Material {
         Albedo = new Vec4f(1, 0, 0, 0);
         DiffuseColor = new Vec3f(0, 0, 0);
         SpecularExponent = 0;
+    }
+    public Material(Material o) {
+        RefractiveIndex = o.RefractiveIndex;
+        Albedo = new Vec4f(o.Albedo);
+        DiffuseColor = new Vec3f(o.DiffuseColor);
+        SpecularExponent = o.SpecularExponent;
     }
 }
