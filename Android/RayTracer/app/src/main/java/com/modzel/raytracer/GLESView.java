f56ea3142910b386b7bd71583935ffb21a3de16d
Łukasz Modzelewski <lukasz.modzelewski1@gmail.com>
2020-10-12 00:01:44 +0200

    Render scene everytime a user taps the screen.

--- Android/RayTracer/app/src/main/java/com/modzel/raytracer/GLESView.java
+++ Android/RayTracer/app/src/main/java/com/modzel/raytracer/GLESView.java
@@ -29,12 +29,14 @@ public class GLESView extends GLSurfaceView {
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-            case MotionEvent.ACTION_MOVE:
+            //case MotionEvent.ACTION_MOVE:
                 // TouchX = (event.getX() - 540.f) / 540.f; // 1080 / 2; normalized X, in range <-1,1>
                 // TouchY = (event.getY() - 1014.f) / 1014.f; // 2029 / 2; normalized Y, in range <-1,1>
-                TouchX = (int)event.getRawX();
-                TouchY = (int)event.getRawY();
-                OpenGLRenderer.PaintCell();
+                // TouchX = (int)event.getRawX();
+                // TouchY = (int)event.getRawY();
+                // RendererInstance.PaintCell();
+                OpenGLRenderer.SceneRenderer.Eye.RotateX((float)Math.PI / 16.f);
+                OpenGLRenderer.SceneRenderer.RenderFrame();
                 break;
             case MotionEvent.ACTION_UP:
                 break;
