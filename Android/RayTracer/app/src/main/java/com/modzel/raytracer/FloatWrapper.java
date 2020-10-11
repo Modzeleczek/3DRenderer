71dbcba2c7baddce850b39d35a255c8c613c3c42
Łukasz Modzelewski <lukasz.modzelewski1@gmail.com>
2020-10-11 03:42:27 +0200

    Add FloatWrapper to simulate passing a float by reference.

---
+++ Android/RayTracer/app/src/main/java/com/modzel/raytracer/FloatWrapper.java
@@ -0,0 +1,13 @@
+package com.modzel.raytracer;
+
+public class FloatWrapper {
+
+    public float Value;
+
+    public FloatWrapper(float value) {
+        Value = value;
+    }
+    public FloatWrapper() {
+        Value = 0.f;
+    }
+}
